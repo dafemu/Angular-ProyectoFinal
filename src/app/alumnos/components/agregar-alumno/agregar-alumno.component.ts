016ba548d9296996d1df88c30d06c9edcaa9ac72
David Munoz Barco <david.f.do@hotmail.com>
2023-03-11 21:53:45 -0500

    new modules

--- src/app/alumnos/components/agregar-alumno/agregar-alumno.component.ts
+++ src/app/alumnos/components/agregar-alumno/agregar-alumno.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Alumno } from 'src/app/alumnos/interfaces/alumno';
+import { AlumnosService } from '../../servicios/alumnos.service';
 
 @Component({
   selector: 'app-agregar-alumno',
@@ -11,7 +12,7 @@ export class AgregarAlumnoComponent {
   formAgregarAlumno: FormGroup;
   @Output()onNuevoAlumno: EventEmitter<Alumno> = new EventEmitter<Alumno>;
 
-  constructor(){
+  constructor(private alumnosService:AlumnosService){
     let soloLetrasRegex:string = '^[a-zA-Z ]*$';
     let controles: any = {
       id: new FormControl('', [Validators.required]),
@@ -25,10 +26,11 @@ export class AgregarAlumnoComponent {
   }
 
   agregarAlumno(){
-    console.log(this.formAgregarAlumno);
-    console.log(this.formAgregarAlumno.value);
+    // console.log(this.formAgregarAlumno);
+    // console.log(this.formAgregarAlumno.value);
     if(this.formAgregarAlumno.status === "VALID"){
-      this.onNuevoAlumno.emit(this.formAgregarAlumno.value);
+      // this.onNuevoAlumno.emit(this.formAgregarAlumno.value);
+      this.alumnosService.agregarAlumno(this.formAgregarAlumno.value)
       this.formAgregarAlumno.reset();
     }
   }
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgregarCursoComponent } from './agregar-curso/agregar-curso.component';
+import { EditarCursoComponent } from './editar-curso/editar-curso.component';
+import { ListarCursosComponent } from './listar-cursos/listar-cursos.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AgregarCursoComponent,
+    EditarCursoComponent,
+    ListarCursosComponent
+  ],
   imports: [
     CommonModule
   ]
